96f103aaccd137de69c56ebdb25607725b932eae
richard hopkins <richard.t.hopkins@outlook.com>
2021-04-05 11:24:24 +1000

    added reset.css and basic bootstap elements.

--- assests/js/script.js
+++ assests/js/script.js
@@ -1,16 +1,11 @@
 // var weather = 'http://api.openweathermap.org/data/2.5/weather?q=dubbo&units=metric&appid=331b26eb68b03536ea7ca4b2ea5009ec';
 // var forcast = 'http://api.openweathermap.org/data/2.5/forecast?q=dubbo&units=metric&appid=331b26eb68b03536ea7ca4b2ea5009ec';
 
-var deafaultLocaion = 'london';
-
-
-
-
 function getWeather(location,unitOfMeasure){
 
-    var weather = 'http://api.openweathermap.org/data/2.5/weather?q=' + location + '&units=metric&appid=331b26eb68b03536ea7ca4b2ea5009ec';
+    var weather = 'http://api.openweathermap.org/data/2.5/weather?q=london&units=metric&appid=331b26eb68b03536ea7ca4b2ea5009ec';
 
-    fetch(forcast)
+    fetch(weather)
         .then(function (response) {
         return response.json();
         })
@@ -20,3 +15,6 @@ function getWeather(location,unitOfMeasure){
 
 
 };
+
+
+getWeather();
