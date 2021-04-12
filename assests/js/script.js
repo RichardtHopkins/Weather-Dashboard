2070afd5e2e238269377a640501f75f54f7ba830
Richard Hopkins <richard.t.hopkins@outlook.com>
2021-04-12 18:15:17 +1000

    added in city searching.

--- assests/js/script.js
+++ assests/js/script.js
@@ -1,9 +1,15 @@
 // var weather = 'http://api.openweathermap.org/data/2.5/weather?q=dubbo&units=metric&appid=331b26eb68b03536ea7ca4b2ea5009ec';
 // var forcast = 'http://api.openweathermap.org/data/2.5/forecast?q=dubbo&units=metric&appid=331b26eb68b03536ea7ca4b2ea5009ec';
 
-function getWeather(location,unitOfMeasure){
+var searchBtn = document.getElementById('searchBtn');
+var cityInput = document.getElementById('cityInput');
+var city;
 
-    var weather = 'http://api.openweathermap.org/data/2.5/weather?q=london&units=metric&appid=331b26eb68b03536ea7ca4b2ea5009ec';
+
+function getWeather(location){
+
+    var weather = 'http://api.openweathermap.org/data/2.5/weather?q=' + location +'&units=metric&appid=331b26eb68b03536ea7ca4b2ea5009ec';
+    var forcast = 'http://api.openweathermap.org/data/2.5/forecast?q='+ location +'&units=metric&appid=331b26eb68b03536ea7ca4b2ea5009ec';
 
     fetch(weather)
         .then(function (response) {
@@ -11,13 +17,55 @@ function getWeather(location,unitOfMeasure){
         })
         .then(function (data) {
         console.log(data)
+
+
+
+        //add object to get data
+
+        })
+        .catch(function (error) {
+            alert('There was an issue with this search');
         });
 
+    fetch(forcast)
+        .then(function (response) {
+        return response.json();
+        })
+        .then(function (data) {
+        console.log(data)
 
+
+
+        //add object for data
+
+        })
+        .catch(function (error) {
+            alert('There was an issue with this search');
+        });
 };
 
 
-getWeather();
+
+searchBtn.addEventListener('click', function(event){
+    event.preventDefault();
+
+    if(cityInput != 0){
+        city = cityInput.value;
+        getWeather(city);
+        console.log(city);
+    }else{
+        alert("You have not entered a city.");
+    };
+})
+
+
+
+
+
+
+
+
+
 
 
 //create object to store search results and save it to local storage.
